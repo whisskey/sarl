5743d99543275b8c89789709108dd1905849de47
Whiskey <Berkoic@hotmail.com>
2024-09-17 22:55:04 +0300

    test(UnsafeLib): add test suite

---
+++ test/ArrayLib.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.26;
+
+import { Test } from "forge-std/src/Test.sol";
+
+contract ArrayLibTest is Test {
+    function setUp() public { }
+
+    function test_arr() public view { }
+}
--- test/UnsafeLib.t.sol
+++ test/UnsafeLib.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.26;
 
 import { Test } from "forge-std/src/Test.sol";
 
-contract LibArrayTest is Test {
+contract UnsafeLibTest is Test {
     function setUp() public { }
 
     function test_arr() public view { }
