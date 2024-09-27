3c731a1d937507f2f8b674e122288fc84a8a9397
Whiskey <Berkoic@hotmail.com>
2024-09-27 21:31:25 +0300

    feat(core): add Array library

---
+++ src/libraries/ArrayLib.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.27;
+
+/// @notice Library for managing solidity dynamic arrays, focusing on optimized memory handling and efficient gas usage.
+/// @author whisskey (https://github.com/whisskey/sarl)
+///
+/// @dev Note: The implemented functions utilize low-level assembly for optimal performance.
+/// The functions have been carefully crafted to manage memory safely and efficiently,
+/// ensuring that the allocated memory is properly freed and resized without leaks or
+/// overflows.
+library ArrayLib { }
